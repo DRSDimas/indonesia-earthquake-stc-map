5c6546fdb15f886c0cf0977f4f9d42238370a5bc
DRSDimas <drsdmk2@gmail.com>
2025-11-20 23:32:08 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+
+  base: '/indonesia-earthquake-stc-map/',
+
   plugins: [
     tailwindcss(),
   ],
